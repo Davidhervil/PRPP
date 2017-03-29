2be8b2ed63af20fd2896acbd43726f132ac1ada5
Arturo Toro <arturoantoniotr@gmail.com>
2017-03-29 01:27:21 -0400

    Update bandb.cpp

--- bandb.cpp
+++ bandb.cpp
@@ -7,11 +7,9 @@
 using namespace std;
 typedef vector<vector<pair <int,int> > > Graph;
 
-/* Funcion que obtiene el beneficio total de una solucion p. 
-*/
 
 /* Recibe par(nodo,beneficio) y lo ordena de mayor a menor*/
-bool comparador (pair <int,int> i,pair <int,int> j) {return (i.second>j.second); }
+bool comparador (pair <int,int> i,pair <int,int> j) {return (i.value>j.value); }
 
 int busqueda(Graph *graph){
 	// VARIABLES
@@ -22,9 +20,8 @@ int busqueda(Graph *graph){
 		mayorBen
 	*/
 	// Locales:
-	vector<pair <int,int>> sucesores;						// Vector de sucesores con sus beneficios.
-	int s,b1,b2,benef,v;											
-	memset(marked,-1,sizeof(marked));						// Inicializar marcados
+	vector<pair <int,int> > sucesores;						// Vector de sucesores con sus beneficios.
+	int s,b1,b2,be,ce,benef,v,e;											
 
 	// ALGORITMO
 	v = solParcial.back();									// El vertice mas externo de la solucion parcial
@@ -34,21 +31,21 @@ int busqueda(Graph *graph){
 		mayorBen = benef;									
 	}
 
-	for(int i=1;i<graph[v].size();i++){						// Crear lista de sucesores
-		s = graph[v][i];
+	for(int i=1;i<(*graph)[v].size();i++){						// Crear lista de sucesores
+		s = (*graph)[v][i].value;
 		if ( s != -1){
-			b1 = benef + graph[s][i].value - graph[s][i].cost;	// calcular beneficio nuevo
-			b2 = benef - graph[s][i].cost						// Calcular beneficio nuevo
+			b1 = benef + (*graph)[v][i].value - (*graph)[v][i].cost;	// calcular beneficio nuevo
+			b2 = benef - (*graph)[v][i].cost;						// Calcular beneficio nuevo
 			sucesores.pb(make_pair(i,b1));						// Agregar vecinos.
 			sucesores.pb(make_pair(i,b2));
 		}
 	}
 	sort(sucesores.begin(), sucesores.end(), comparador);		// Ordenar sucesores de mayor a menor.
 
-	for(int i=1; i<sucesores.size(); i++){					// Recorrer desde el ultimo.
-		e = sucesores[i].first(); 							// Aqui esta el nodo a verificar (Bueno, la arista)
-		be = graph[v][e].value;
-		ce = graph[v][e].cost;
+	for(int i=0; i<sucesores.size(); i++){					// Recorrer desde el ultimo.
+		e  = sucesores[i].first(); 							// Aqui esta el nodo a verificar (Bueno, la arista)
+		be = (*graph)[v][e].value;
+		ce = (*graph)[v][e].cost;
 		if(verificaciones){		 
 			solParcial.pb(e);	 							// Agregar a la solucion parcial.
 			beneficioDisponible -= max(0,be-ce);
@@ -62,11 +59,11 @@ int busqueda(Graph *graph){
 int bandb(Graph *G, vector<int> solInicial, int benInicial){
 	// PONER ESTO COMO VARIABLES GLOBALES //
 	int beneficioDisponible, mayorBen;
-	vector<int> solParcial (1,1)
+	vector<int> solParcial (1,1);
 	vector<int> mejorSol (solInicial);
 	//-----------------------------------//
 	mayorBen = benInicial;
-	beneficioDisponible = mayor_ben_grafo(G)		///// ??????
-	busqueda(G)										///// ??????
+	beneficioDisponible = mayor_ben_grafo(G);		///// ??????
+	busqueda(G);										///// ??????
 }
 
