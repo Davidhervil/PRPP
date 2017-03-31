b0671e1846133557930c070da1e901926b006160
Arturo Toro <arturoantoniotr@gmail.com>
2017-03-31 00:38:58 -0400

    Update bandb.cpp

--- bandb.cpp
+++ bandb.cpp
@@ -1,11 +1,3 @@
-#include <iostream>
-#include <stdio.h>
-#include <stack>
-#include <vector>
-#include <string.h>
-#include <algorithm>
-using namespace std;
-typedef vector<vector<pair <int,int> > > Graph;
 
 bool esta_lado_en_sol_parcial(pair<int,int> e, int be){
 	int marked[110][110];
@@ -32,10 +24,12 @@ bool esta_lado_en_sol_parcial(pair<int,int> e, int be){
 
 bool cumple_acota(Graph *graph, int v, int e,int be, int ce, int benef){
 	int beneficioE, beneficioSolP, beneficioMax;
-	beneficioE    = be-ce;
-	beneficioSolP = benef + beneficioE;
-	beneficioMax  = beneficioDisponible - max(0,be-ce) + beneficioSolP;
-	return beneficioMax > mayorBen;
+	bool b;
+	beneficioE    = be-ce;					// Ganancia de recorrer la arista
+	beneficioSolP = benef + beneficioE;		// 
+	beneficioMax  = beneficioDisponible - max(0,beneficioE) + beneficioSolP;
+	b = beneficioMax> mayorBen;
+	return b;
 }
 
 /* Recibe par(nodo,beneficio) y lo ordena de mayor a menor*/
@@ -51,54 +45,65 @@ int busqueda(Graph *graph){
 	*/
 	// Locales:
 	vector<pair <int,int> > sucesores;						// Vector de sucesores con sus beneficios.
-	int s,b1,b2,be,ce,benef,v,e;											
-
+	int s,b1,b2,be,ce,benef,v,e, ultimo, penultimo;											
+	for(int i=0;i<solParcial.size();i++)cout<<solParcial[i]<<' ';cout<<endl;
 	// ALGORITMO
 	v = solParcial.back();									// El vertice mas externo de la solucion parcial
-	benef = profit(solParcial,&graph)						// Hallar beneficio actual (MEJORABLE)
+	benef = profit(solParcial,graph);						// Hallar beneficio actual (MEJORABLE)
 	if(v == 1 && benef > mayorBen){							// Reasignar mejor solucion
 		mejorSol = solParcial;
 		mayorBen = benef;									
 	}
-
+	countBusq += 1;
 	for(int i=1;i<(*graph)[v].size();i++){						// Crear lista de sucesores
 		s = (*graph)[v][i].value;
 		if ( s != -1){
 			b1 = benef + (*graph)[v][i].value - (*graph)[v][i].cost;	// calcular beneficio nuevo
 			b2 = benef - (*graph)[v][i].cost;						// Calcular beneficio nuevo
+			if(v==2){
+			}
 			sucesores.pb(make_pair(i,b1));						// Agregar vecinos.
 			sucesores.pb(make_pair(i,b2));
 		}
 	}
-	sort(sucesores.begin(), sucesores.end(), comparador);		// Ordenar sucesores de mayor a menor.
 
+	sort(sucesores.begin(), sucesores.end(), comparador);	// Ordenar sucesores de mayor a menor.
 	for(int i=0; i<sucesores.size(); i++){					// Recorrer desde el ultimo.
 		e  = sucesores[i].first; 							// Aqui esta el nodo a verificar (Bueno, la arista)
 		be = sucesores[i].second;
 		ce = (*graph)[v][e].cost;
-		if(!cumple_acota(graph,v,e,be,ce,benef) &&
+		if(cumple_acota(graph,v,e,be,ce,benef) &&
 			!esta_lado_en_sol_parcial(make_pair(v,e),be)) {		 
 			solParcial.pb(e);	 							// Agregar a la solucion parcial.
 			beneficioDisponible -= max(0,be-ce);
-			busqueda(G);									// ?????????????????
+			busqueda(graph);	
+			beneficioDisponible += max(0,be-ce);								// ?????????????????
 		}
 	}
-	beneficioDisponible += max(0,be-ce);
-	sucesores.pop_back();
-
+	if (solParcial.size()!=0){
+		countBusq -= 1;
+		ultimo = solParcial.back();
+		solParcial.pop_back();
+	}
 }
-int bandb(Graph *G, vector<int> solInicial, int benInicial){
-	// PONER ESTO COMO VARIABLES GLOBALES //
-	int beneficioDisponible, mayorBen;
-	vector<int> solParcial (1,1);
-	vector<int> mejorSol (solInicial);
-	//-----------------------------------//
+
+int mayor_ben_grafo(Graph *graph){
+	int r=0;
+	for(int i=0;i<(*graph)[0].size();i++){
+		for(int j=0;j<(*graph)[0].size();j++){
+			r += ((*graph)[i][j].value)/2;
+		}
+	}
+	return r;
+}
+int bandb(Graph *G, vector<int> *solInicial, int benInicial){
+	mejorSol = (*solInicial);
 	mayorBen = benInicial;
 	beneficioDisponible = mayor_ben_grafo(G);		///// ??????
 	busqueda(G);										///// ??????
+		cout<<"chao busqueda"<<endl;
 	cout<<mayorBen<<endl;
-	for(int i=0;i<solParcial.size();i++)cout<<solParcial[i]<<' ';
+	for(int i=0;i<mejorSol.size();i++)cout<<mejorSol[i]<<' ';
 		cout<<endl;
 }
-
 
